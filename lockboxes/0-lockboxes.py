fb91790b86c2f2172578c4b09bcd6c6fec910549
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-05-19 18:39:02 +0200

    Add: function 0-lockboxes, return true if all boxe give a key on the index of each boxe of the list of boxes tested

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+
+    nboxes = len(boxes)
+    found_index = set([0])
+    keys = [0]
+
+    while keys:
+        curr_box = keys.pop()
+
+        for next_key in boxes[curr_box]:
+            if next_key < nboxes and next_key not in found_index:
+                found_index.add(next_key)
+                keys.append(next_key)
+
+    return len(found_index) == nboxes
