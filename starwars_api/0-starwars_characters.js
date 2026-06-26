c710445dd1c4b8232166e56a15831471fa4fc86b
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-06-26 15:38:59 +0200

    Add: stars wars algorithm

---
+++ starwars_api/0-starwars_characters.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+const request = require('request');
+const movieId = process.argv[2];
+const filmURL = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+
+function printCharacters (characters, index) {
+  if (index >= characters.length) {
+    return;
+  }
+  request(characters[index], (error, response, body) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+    console.log(JSON.parse(body).name);
+    printCharacters(characters, index + 1);
+  });
+}
+
+request(filmURL, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  const characters = JSON.parse(body).characters;
+  printCharacters(characters, 0);
+});
---
+++ starwars_api/README.md
@@ -0,0 +1,13 @@
+# Star Wars API
+
+Scripts that interact with the Star Wars API (swapi-api.hbtn.io).
+
+## Scripts
+
+- `0-starwars_characters.js` - Prints all characters of a Star Wars movie, given its Movie ID as the first positional argument.
+
+## Requirements
+
+- Node 10.14.x
+- `request` module
+- semistandard compliant code
