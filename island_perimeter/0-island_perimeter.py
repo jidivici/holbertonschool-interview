2f68e0d537c8fa2433647023576a4b40f9864079
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-07-08 15:54:22 +0200

    Add: island perimeter found the perimeter of island by square

---
+++ island_perimeter/0-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""0-island_perimeter.py - version brute force"""
+
+
+def island_perimeter(grid):
+    """Calcule le périmètre de l'île dans grid."""
+    if not grid or not grid[0]:
+        return 0
+
+    rows = len(grid)
+    cols = len(grid[0])
+    perimeter = 0
+
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == 1:
+                perimeter += 4
+                if r > 0 and grid[r - 1][c] == 1:
+                    perimeter -= 1
+                if r < rows - 1 and grid[r + 1][c] == 1:
+                    perimeter -= 1
+                if c > 0 and grid[r][c - 1] == 1:
+                    perimeter -= 1
+                if c < cols - 1 and grid[r][c + 1] == 1:
+                    perimeter -= 1
+    return perimeter
