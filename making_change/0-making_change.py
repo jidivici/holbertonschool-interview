8c857dd126d31ddac66d3d118ba4173a04c58640
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-07-08 15:57:33 +0200

    Add: make_change determine the fewest number of coins needed to meet a given amount total

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -1,21 +1,10 @@
 #!/usr/bin/python3
-"""
-Making change
-"""
+"""Making change"""
 
 
 def makeChange(coins, total):
     """
     Determine the fewest number of coins needed to meet a given amount total
-
-    Args:
-        coins (list): list of the values of the coins in your possession
-        total (int): the amount to meet
-
-    Returns:
-        int: fewest number of coins needed to meet total
-             0 if total is 0 or less
-             -1 if total cannot be met by any number of coins
     """
     if total <= 0:
         return 0
